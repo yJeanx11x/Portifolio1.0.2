e9409cd6a3a1d71cf070e17053dda034aa9392a3
Jean Lucas <j3anlucas010@gmail.com>
2024-05-29 20:39:58 -0300

    Add redes de contatos

--- src/scripts/script.js
+++
@@ -1,9 +0,0 @@
-let imgs = document.querySelector(".fotoPrincipal");
-function trocarDeFoto() {
-  imgs.addEventListener("click", function() {
-    imgs.classList.remove("fotoPrincipal");
-    imgs.classList.add("fotoPrincipal2");
-
-});
-}
-trocarDeFoto();
